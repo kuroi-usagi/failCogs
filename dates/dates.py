034fa283848bc9d546e1ffc587eb1c2d5ca5916a
Schütz, Marcus <marcus.schuetz@immonet.de>
2017-07-25 13:37:08 +0200

    Embed designing

--- dates/dates.py
+++ dates/dates.py
@@ -50,13 +50,15 @@ class Dates:
 
         server = ctx.message.server
         dates = self.dates[server.id]
-        embed = discord.Embed(title="Termine", color = 0x00c9f4)
+        embed = discord.RichEmbed(title="Termine", color = 0x00c9f4)
+        embed.setTimestamp()
         datetext = ""
         for date in dates:
             header = date
             for time in self.dates[server.id][date]:
                 datetext += "*" + time + "* - " + self.dates[server.id][date][time] + "\n"
             embed.add_field(name=header, value=datetext)
+            embed.addBlankField(True)
             datetext = ""
         await self.bot.say(embed=embed)
 
