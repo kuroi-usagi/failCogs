fcd7b47dab630770bfb660a23759e5ff9b44430b
Schütz, Marcus <marcus.schuetz@immonet.de>
2017-07-25 16:06:51 +0200

    removed async method

--- dates/dates.py
+++ dates/dates.py
@@ -76,19 +76,19 @@ class Dates:
         if await self._delete_date(server.id, date, time):
             await self.bot.say("Termin gelöscht.")
 
-    async def _delete_date(self,serverId, date, time):
+     def _delete_date(self,serverId, date, time):
         print("deleting date entry")
         deletedState = False
-        # if serverId in self.dates:
-        #     if date in self.dates[serverId]:
-        #         if time in self.dates[serverId][date]:
-        #             del self.dates[serverId][date][time]
-        #             deletedState = True
-        # if not self.dates[serverId][date]:
-        #     del self.dates[serverId][date]
-        #
-        # dataIO.save_json(self.dates_path, self.dates)
-        # self.dates = dataIO.load_json(self.dates_path)
+        if serverId in self.dates:
+            if date in self.dates[serverId]:
+                if time in self.dates[serverId][date]:
+                    del self.dates[serverId][date][time]
+                    deletedState = True
+        if not self.dates[serverId][date]:
+            del self.dates[serverId][date]
+
+        dataIO.save_json(self.dates_path, self.dates)
+        self.dates = dataIO.load_json(self.dates_path)
 
         return deletedState
 
@@ -111,7 +111,7 @@ class Dates:
                             print(serverId)
                             print(date)
                             print(time)
-                            await self._delete_date(serverId, date, time)
+                            self._delete_date(serverId, date, time)
 
     def checkDateTime(self, date, time):
         try:
