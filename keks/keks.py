fea43f39e6047eec0037b3bce3e4f8859a94ee02
Schütz, Marcus <marcus@failgod.de>
2017-08-16 13:31:58 +0200

    Added keks command

---
+++ keks/keks.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+import random
+
+
+class Keks:
+    """
+    The Keks Cog
+    """
+    def __init__(self, bot):
+        self.bot = bot
+        self.cookie_answers = [
+            "Oh vielen Dank!",
+            "Om nom nom",
+            "Ich liebe Kekse!"
+        ]
+
+    @commands.command(name="keks", pass_context=True)
+    async def _give_cookie(self, ctx):
+        """
+        Give the bot a cookie
+        """
+        await self.bot.say(ramdon.choice(self.cookie_answers))
+
+def setup(bot):
+    n = Keks(bot)
+    bot.add_cog(n)
